004f7fc53423b1210f75a39bd32c06ccb016fa16
John Christensen <johnawesomejr@gmail.com>
2023-04-17 06:48:46 -0500

    more

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -5,8 +5,18 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/spacelab/bootstrap.min.css">
 </head>
 <body>
+<style>
+#production-bannder{
+	color:white;
+	background:red;
+	position:fixed;
+	top:0px;
+	left:0px;
+	width:100%;
+}
+</style>
 @if (env('APP_ENV') === 'production')
-    <p style="color:red;">This is the production environment.</p>
+    <p id="production-banner">This is the production environment.</p>
 @else
     <p>This is not the production environment.</p>
 @endif
