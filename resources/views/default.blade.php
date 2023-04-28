9de8f6a7cd295c82fdbbef6b2912fc348e942dd1
John Christensen <johnawesomejr@gmail.com>
2023-04-27 21:09:16 -0500

    added back in the bootstrap css cdn

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/spacelab/bootstrap.min.css">
 </head>
 <body>
 <style>
