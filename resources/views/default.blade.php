d57d77b6bac3d0541466b778f62a6e4fb2026e5b
John Christensen <johnawesomejr@gmail.com>
2023-05-03 00:56:36 -0500

    changed the theme

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/spacelab/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/flatly/bootstrap.min.css" integrity="sha384-qF/QmIAj5ZaYFAeQcrQ6bfVMAh4zZlrGwTPY7T/M+iTTLJqJBJjwwnsE5Y0mV7QK" crossorigin="anonymous">
 </head>
 
 <body>
@@ -25,6 +25,9 @@
             color: gray;
             background: lightgray;
         }
+        .btn-danger{
+            color: darkred;
+        }
     </style>
     @if (env('APP_ENV') === 'production')
         <p id="production-banner" class="banner-style-pro">This is the production environment.</p>
@@ -35,14 +38,22 @@
     @endif
 
     <x-app-layout>
-        <x-slot name="header">
+        {{-- <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Dashboard') }}
             </h2>
-        </x-slot>
+        </x-slot> --}}
 
         <div class="container">
-            @yield('content')
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </x-app-layout>
 </body>
